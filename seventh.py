52899e3311817fb95cad4183122417d1ef6efe46
dan4a <dan4a06@mail.ru>
2025-09-24 00:09:50 +0400

    new commit

---
+++ seventh.py
@@ -0,0 +1,41 @@
+#1 ---------------
+#people = {
+#    "John": 25,
+#    "Emma": 30,
+#    "Michael": 22,
+#    "Sophia": 28,
+#    "David": 35,
+#    "Olivia": 26
+#}
+#
+#N = int(input('put age: '))
+#
+#for key in people:
+#    if people[key] > N:
+#        print(key)
+
+#2 ----------------
+#people = {
+#    "25": "Jonh",
+#    "30": "Emma",
+#    "22": "Michael",
+#    "28": "Sophia",
+#    "35": "David",
+#    "26": "Olivia",
+#}
+#
+#N = int(input('put age: '))
+
+#for key in people:
+#    if int(key) > N:
+#        print(people[key])
+
+#3 -------
+people = {
+	"25": "Jonh",
+	"30": "Emma",
+	"22": "Michael",
+	"28": "Sophia",
+	"35": "David",
+	"26": "Olivia",
+
